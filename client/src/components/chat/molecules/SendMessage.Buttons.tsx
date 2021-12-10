4240c1f67d521296586509574999ff1c1f4875f3
yago biermann <bsfyagobsf@gmail.com>
2021-12-10 17:28:20 -0300

    fix: lift up record component

--- client/src/components/chat/molecules/SendMessage.Buttons.tsx
+++ client/src/components/chat/molecules/SendMessage.Buttons.tsx
@@ -1,12 +1,8 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Box from '../../global/Box'
 import styled from 'styled-components'
 import EmojiButton from '../atoms/Buttons.Emoji'
-import SendButton from '../atoms/Buttons.Send'
-import RecordButton from '../atoms/Buttons.Record'
 import UploadButton from '../atoms/Buttons.Upload'
-import useRecorder from '../../../lib/hooks/useRecorder'
-import RecordAudio from './RecordAudio'
 
 const MainBox = styled(Box)`
   flex-direction: row;
@@ -19,27 +15,15 @@ const MainBox = styled(Box)`
 `
 
 function SendMessageButtons(props: any) {
-  const { startRecord, finishRecord, cancelRecord, recorder } = useRecorder()
-
   return (
     <>
-      {recorder.isRecording ? (
-        <RecordAudio
-          finish={finishRecord}
-          cancel={cancelRecord}
-          data={recorder.audio}
-        />
-      ) : (
-        <MainBox>
-          {props.hasMessage ? (
-            <SendButton type="submit" />
-          ) : (
-            <RecordButton onClick={startRecord} />
-          )}
-          <EmojiButton />
-          <UploadButton />
-        </MainBox>
-      )}
+      (
+      <MainBox>
+        {props.children}
+        <EmojiButton />
+        <UploadButton />
+      </MainBox>
+      )
     </>
   )
 }
--- client/src/components/chat/molecules/SendMessage.tsx
+++ client/src/components/chat/molecules/SendMessage.tsx
@@ -6,7 +6,10 @@ import { useForm } from 'react-hook-form'
 import { useAppDispatch, useAppSelector } from '../../../store'
 import { sendMessage } from '../../../services/messages'
 import { MESSAGE_TYPE } from '../../../lib/enums'
-import socket from '../../../services/sockets'
+import SendButton from '../atoms/Buttons.Send'
+import RecordButton from '../atoms/Buttons.Record'
+import useRecorder from '../../../lib/hooks/useRecorder'
+import RecordAudio from './RecordAudio'
 
 const MessageForm = styled.form`
   display: flex;
@@ -17,23 +20,23 @@ const MessageForm = styled.form`
 
 function SendMessage() {
   const dispatch = useAppDispatch()
+  const { cancelRecord, finishRecord, recorder, startRecord } = useRecorder()
   const { register, handleSubmit, watch, resetField } =
     useForm<{ message: string }>()
-
   const message = watch('message', '')
+
+  //Send text message
   const handleSubmitMessage = (data: string) => {
     resetField('message')
     dispatch(sendMessage({ data, type: MESSAGE_TYPE.TEXT }))
   }
 
+  //send audio message
   useEffect(() => {
-    socket.on('message', message => {
-      console.log(message)
-    })
-    return () => {
-      socket.off('message')
+    if (recorder.audio) {
+      dispatch(sendMessage({ data: recorder.audio, type: MESSAGE_TYPE.AUDIO }))
     }
-  })
+  }, [recorder.audio])
 
   return (
     <MessageForm
@@ -41,7 +44,17 @@ function SendMessage() {
       onSubmit={handleSubmit(data => handleSubmitMessage(data.message))}
     >
       <InputMessage {...register('message')} />
-      <SendMessageButtons hasMessage={Boolean(message)} />
+      {recorder.isRecording ? (
+        <RecordAudio finish={finishRecord} cancel={cancelRecord} />
+      ) : (
+        <SendMessageButtons>
+          {message ? (
+            <SendButton type="submit" />
+          ) : (
+            <RecordButton onClick={startRecord} />
+          )}
+        </SendMessageButtons>
+      )}
     </MessageForm>
   )
 }
