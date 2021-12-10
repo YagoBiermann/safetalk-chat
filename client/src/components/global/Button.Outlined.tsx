ff3940c121aee3ff719829ced43db91171209b20
yago biermann <bsfyagobsf@gmail.com>
2021-12-10 17:29:40 -0300

    style: change style of outlined button

--- client/src/components/global/Button.Outlined.tsx
+++ client/src/components/global/Button.Outlined.tsx
@@ -4,13 +4,16 @@ import { styled } from '@mui/material/styles'
 
 const StyledButton = styled(IconButton)(
   ({ theme }) => `
-    background-color: transparent;
     border: 2px solid ${theme.palette.primary.main};
     height: min-content;
     transition: ease-in-out 0.2s;
     &:hover {
       background-color: ${theme.palette.primary.light};
+      opacity: 0.8;
       transform: scale(1.2);
+    }
+    & > svg {
+      color: whitesmoke;
     }
 
   `
