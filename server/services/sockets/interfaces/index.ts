cf6171e998e6313a613673e1d6b37d2fc3a9ad1a
yago biermann <bsfyagobsf@gmail.com>
2021-12-17 14:05:18 -0300

    feat: add interface for messages

--- server/services/sockets/interfaces/index.ts
+++ server/services/sockets/interfaces/index.ts
@@ -1,9 +1,17 @@
-export interface IMessage {
+export interface Message {
   id: string
   username: string
   roomCode: string
-  data: string | Blob
+  message: string
   type: MESSAGE_TYPE
+}
+
+export interface AudioMessage extends Omit<Message, 'message'> {
+  audio: Blob | string | Buffer
+}
+export interface FileMessage extends Omit<Message, 'message'> {
+  file: Blob | Buffer | string
+  message?: string
 }
 
 export enum MESSAGE_TYPE {
