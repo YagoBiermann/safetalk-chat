42493c29dedd5d69416c285358af22652881f841
yago biermann <bsfyagobsf@gmail.com>
2021-12-17 14:22:41 -0300

    refactor: add app reducer and remove error reducer

--- client/src/store/ducks/rootReducer.ts
+++ client/src/store/ducks/rootReducer.ts
@@ -1,14 +1,14 @@
 import { combineReducers } from '@reduxjs/toolkit'
 import { userSlice } from './users'
 import { roomSlice } from './rooms'
-import { errorSlice } from './error'
 import { roomApi } from '../../services/api'
 import { messagesSlice } from './messages'
+import { appSlice } from './app'
 
 const rootReducer = combineReducers({
   user: userSlice.reducer,
   room: roomSlice.reducer,
-  error: errorSlice.reducer,
+  app: appSlice.reducer,
   message: messagesSlice.reducer,
   [roomApi.reducerPath]: roomApi.reducer
 })
