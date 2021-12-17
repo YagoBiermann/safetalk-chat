131b4153e9d4821d464f6dd33e6bde1f651559af
yago biermann <bsfyagobsf@gmail.com>
2021-12-17 14:20:14 -0300

    feat: add new actions to message reducer

--- client/src/store/ducks/messages/index.ts
+++ client/src/store/ducks/messages/index.ts
@@ -1,14 +1,21 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
-import { Message } from '../../../lib/interfaces'
+import { AudioMessage, FileMessage, Message } from '../../../lib/interfaces'
 
-const initialState: Message[] = []
+const initialState: Array<Message | AudioMessage | FileMessage> = []
 export const messagesSlice = createSlice({
   name: 'messages',
   initialState: initialState,
   reducers: {
-    addMessage: (state, action: PayloadAction<Message>) => {
+    addTextMessage: (state, action: PayloadAction<Message>) => {
+      state.push(action.payload)
+    },
+    addAudioMessage: (state, action: PayloadAction<AudioMessage>) => {
+      state.push(action.payload)
+    },
+    addFileMessage: (state, action: PayloadAction<FileMessage>) => {
       state.push(action.payload)
     }
   }
 })
-export const { addMessage } = messagesSlice.actions
+export const { addTextMessage, addAudioMessage, addFileMessage } =
+  messagesSlice.actions
