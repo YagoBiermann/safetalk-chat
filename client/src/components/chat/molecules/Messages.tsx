5c0c4c5a7286e8d83e84af7ee3a96802e6e3706c
yago biermann <bsfyagobsf@gmail.com>
2021-12-17 15:04:15 -0300

    feat: add event to show dropzone on chat

--- client/src/components/chat/molecules/Messages.tsx
+++ client/src/components/chat/molecules/Messages.tsx
@@ -1,17 +1,46 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useRef, useEffect, useState } from 'react'
 import styled from 'styled-components'
 import Box from '../../global/Box'
+import Dropzone from './Dropzone'
+import DarkenBackground from '../../global/DarkenBackground'
+import Preview from './Dropzone.Preview'
+import { DropFile } from '../../../lib/interfaces'
 
 const OuterBox = styled(Box)`
   align-items: flex-start;
-  background-color: ${props => props.theme.colors.secondaryDark};
+  background-color: ${props => props.theme.colors.secondary.dark.elevation_0};
   border-radius: 25px 5px 5px 5px;
   height: 80vh;
   width: inherit;
+  position: relative;
 `
 
 function MessagesBox() {
-  return <OuterBox></OuterBox>
+  const [isDragOver, setDragOver] = useState(false)
+  const dropzoneRef = useRef<HTMLDivElement | null>(null)
+
+  return (
+    <OuterBox
+      onDragEnter={e => {
+        setDragOver(true)
+      }}
+      onDragLeave={e => {
+        if (e.relatedTarget !== dropzoneRef.current) {
+          setDragOver(false)
+        }
+      }}
+    >
+      {isDragOver ? (
+        <Dropzone
+          open={setDragOver}
+          render={(files: DropFile[]) => (
+            <Preview open={setDragOver} files={files} />
+          )}
+          ref={dropzoneRef}
+        />
+      ) : null}
+    </OuterBox>
+  )
 }
 
 export default MessagesBox
--- client/src/components/chat/molecules/SendMessage.Buttons.tsx
+++ client/src/components/chat/molecules/SendMessage.Buttons.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import Box from '../../global/Box'
 import styled from 'styled-components'
 import EmojiButton from '../atoms/Buttons.Emoji'
