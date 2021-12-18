5d02629bc6796eb079f9192c75bf611c46687ec0
yago biermann <bsfyagobsf@gmail.com>
2021-12-18 05:31:33 -0300

    style: change background color

--- client/src/components/global/DarkenBackground.tsx
+++ client/src/components/global/DarkenBackground.tsx
@@ -2,17 +2,16 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Background = styled.div`
-  position: fixed;
+  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   top: 0;
   right: 0;
   height: 100vh;
   width: 100vw;
   z-index: 999;
-  background: linear-gradient(
-    203deg,
-    rgb(135, 0, 90) 0%,
-    rgba(34, 6, 45, 0.6894958667060574) 0%
-  );
+  background: rgba(13, 13, 13, 0.4);
 `
 
 function DarkenBackground(props: any) {
