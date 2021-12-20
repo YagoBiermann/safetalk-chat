d395bd4f60f925f29b075c497dbe9e6c69825ecd
yago biermann <bsfyagobsf@gmail.com>
2021-12-20 14:52:09 -0300

    fix: remove input uppercase

--- client/src/components/code/atoms/CreateRoom.CodeInput.tsx
+++ client/src/components/code/atoms/CreateRoom.CodeInput.tsx
@@ -5,7 +5,6 @@ import CodeInputStyle from '../../../assets/styles/default.CodeInput'
 
 const StyledInput = styled(PrimaryInput)`
   ${CodeInputStyle}
-  text-transform: uppercase;
 `
 
 function CodeInput(props: { roomCode: string }) {
