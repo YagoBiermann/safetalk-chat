b6c7ee943360f4d8147b3fc351492f9c718e9633
yago biermann <bsfyagobsf@gmail.com>
2021-12-22 15:21:46 -0300

    feat: add allowed type of files

--- client/src/lib/enums/index.ts
+++ client/src/lib/enums/index.ts
@@ -23,3 +23,23 @@ export enum ENDPOINTS {
   BACKEND_URL = 'http://localhost:80/api/v2/',
   FRONTEND_URL = 'http://localhost:80/'
 }
+
+//accepted file types for upload
+export const acceptedTypes = `
+  image/jpeg, 
+  image/png, 
+  video/webm, 
+  video/ogg, 
+  video/mp4, 
+  video/wmv, 
+  video/avi, 
+  video/mov,
+  audio/mp3,
+  audio/mp4, 
+  text/plain, 
+  application/pdf, 
+  application/zip, 
+  application/vnd.openxmlformats-officedocument.wordprocessingml.document, 
+  application/msword, application/vnd.openxmlformats-officedocument.presentationml.presentation, 
+  application/vnd.ms-powerpoint
+`
