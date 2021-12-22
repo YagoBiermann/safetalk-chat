cdb3c9f9ded05bc3a5200fb03cdd1014964bc7aa
yago biermann <bsfyagobsf@gmail.com>
2021-12-22 15:31:19 -0300

    refactor: remove close button from FilePreview.tsx and add to new component

---
+++ client/src/components/chat/atoms/Preview.closeButton.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import OutlinedButton from '../../global/Button.Outlined'
+import Close from '@material-ui/icons/Close'
+import styled from 'styled-components'
+
+const CloseButton = styled(OutlinedButton)`
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  border: none;
+  z-index: 9999;
+  &:hover {
+    transform: scale(1);
+  }
+  & > svg {
+    color: ${props => props.theme.fontColor.secondary};
+  }
+  & > svg:hover {
+    color: ${props => props.theme.fontColor.primary};
+  }
+`
+
+const PreviewCloseButton: React.FC<
+  React.ButtonHTMLAttributes<HTMLButtonElement>
+> = props => {
+  return (
+    <CloseButton {...props}>
+      <Close fontSize="large" />
+    </CloseButton>
+  )
+}
+
+export default PreviewCloseButton
