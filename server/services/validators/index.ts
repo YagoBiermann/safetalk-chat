6a2f4119583b20561d5fb7a8241fbd1cf74f1dc0
yago biermann <bsfyagobsf@gmail.com>
2021-12-24 22:27:41 -0300

    feat: add instance of file and header validators

--- server/services/validators/index.ts
+++ server/services/validators/index.ts
@@ -2,10 +2,14 @@ import { RoomValidator } from './room/RoomValidator'
 import { UserValidator } from './user/UserValidator'
 import { RoomRepository } from '../../database/repositories/RoomRepository'
 import { UserRepository } from '../../database/repositories/UserRepository'
+import { FileValidator } from './request/FileValidator'
+import { HeaderValidator } from './request/HeaderValidator'
 
+const fileValidator = new FileValidator()
+const headerValidator = new HeaderValidator()
 const roomRepository = new RoomRepository()
 const userRepository = new UserRepository()
 const roomValidator = new RoomValidator(roomRepository, userRepository)
 const userValidator = new UserValidator(userRepository)
 
-export { roomValidator, userValidator }
+export { roomValidator, userValidator, fileValidator, headerValidator }
