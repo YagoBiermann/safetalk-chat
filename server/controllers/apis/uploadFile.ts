cf59f54ea3e32dcaf66e17960e1f9c7d66597732
yago biermann <bsfyagobsf@gmail.com>
2021-12-25 07:18:11 -0300

    fix: add mime type to response

--- server/controllers/apis/uploadFile.ts
+++ server/controllers/apis/uploadFile.ts
@@ -2,7 +2,9 @@ import { Request, Response, NextFunction } from 'express'
 
 const uploadFile = (req: Request, res: Response, next: NextFunction) => {
   try {
-    res.status(201).json({ fileName: req.file.filename })
+    res
+      .status(201)
+      .json({ fileName: req.file.filename, mimeType: req.file.mimetype })
   } catch (error) {
     next(error)
   }
