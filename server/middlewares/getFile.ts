c08980f3d8e6f1fdcbe41f3e5fdbfad6af2c3d06
yago biermann <bsfyagobsf@gmail.com>
2021-12-24 22:19:58 -0300

    feat: add validation to get file endpoint

---
+++ server/middlewares/getFile.ts
@@ -0,0 +1,25 @@
+import { Request, Response, NextFunction } from 'express'
+import { IFileUpload } from '../routes/interfaces'
+import { fileValidator, headerValidator } from '../services/validators'
+import { roomValidator } from '../services/validators'
+import { validateRoomCode } from '../services/validators/request'
+
+const validateBeforeGetFile = async (
+  req: Request<IFileUpload>,
+  res: Response,
+  next: NextFunction
+) => {
+  const { roomCode, file } = req.params
+  try {
+    headerValidator.checkContentType(req.headers['content-type'])
+    fileValidator.checkFilePath(file)
+    fileValidator.checkFileExtension(file)
+    validateRoomCode(roomCode)
+    roomValidator.checkIfRoomDoesNotExists(roomCode)
+    next()
+  } catch (error) {
+    next(error)
+  }
+}
+
+export { validateBeforeGetFile }
