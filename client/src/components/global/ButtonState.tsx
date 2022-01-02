8e4eaa199ff3b6451f3fab2c3f3ae9bfb3686f12
yago biermann <bsfyagobsf@gmail.com>
2022-01-02 06:07:13 -0300

    style: add bold property to regular text

--- client/src/components/global/ButtonState.tsx
+++ client/src/components/global/ButtonState.tsx
@@ -6,7 +6,7 @@ function ButtonState(props: { text: string; loading: boolean }) {
   return props.loading ? (
     <CircularProgress size={18} thickness={8} color="inherit" />
   ) : (
-    <RegularText>{props.text}</RegularText>
+    <RegularText bold>{props.text}</RegularText>
   )
 }
 
--- client/src/components/global/RegularText.tsx
+++ client/src/components/global/RegularText.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const Text = styled.span`
+const Text = styled.span<{ bold?: boolean }>`
   color: ${props => props.theme.fontColor.secondary};
   font-size: ${props => props.theme.fontSizes.medium};
-  font-weight: 400;
+  font-weight: ${props => (props.bold ? 600 : 400)};
 `
 
-const RegularText = (props: { children: string }) => {
-  return <Text>{props.children}</Text>
+const RegularText = (props: { children: string; bold?: boolean }) => {
+  return <Text bold={props.bold}>{props.children}</Text>
 }
 
 export default RegularText
