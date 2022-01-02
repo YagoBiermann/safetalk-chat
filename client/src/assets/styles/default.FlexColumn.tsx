9eefbd54ec4c94d2970320d84e1e77abeb9983a1
yago biermann <bsfyagobsf@gmail.com>
2022-01-02 06:03:26 -0300

    style: add common flex properties to default styles

---
+++ client/src/assets/styles/default.FlexColumn.tsx
@@ -0,0 +1,9 @@
+import { css } from 'styled-components'
+
+const CenteredColumn = css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
+export default CenteredColumn
---
+++ client/src/assets/styles/default.FlexRow.tsx
@@ -0,0 +1,10 @@
+import { css } from 'styled-components'
+
+const CenteredRow = css`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`
+
+export default CenteredRow
