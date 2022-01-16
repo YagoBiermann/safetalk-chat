6b3016557fd42e289c8b177ff0e23539271294d4
Yago Biermann <bsfyagobsf@gmail.com>
2022-01-16 03:12:13 -0300

    refactor: change namespace and set autoConnect to false

--- client/src/services/sockets/index.ts
+++ client/src/services/sockets/index.ts
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client'
 import { ENDPOINTS } from '../../lib/enums'
 
-const socket = io(`${ENDPOINTS.FRONTEND_URL}`, {
+const socket = io(`${ENDPOINTS.FRONTEND_URL}chat`, {
   transports: ['websocket', 'polling'],
   path: '/socket.io'
 })
