940853a7e079d10729e4011d83e419f0f193740b
Yago Biermann <bsfyagobsf@gmail.com>
2022-03-09 23:27:07 -0300

    feat: add implementation of auth service

---
+++ server/src/infrastructure/jwt/AuthenticationFactory.ts
@@ -0,0 +1,16 @@
+import AccessKeyValidation from '../../application/validations/leaf/AccessKeyValidation'
+import { IAuthenticationService } from '../../domain/models/auth/AuthenticationService'
+import UserRepositoryFactory from '../database/repositories/factories/UserRepository'
+import Authentication from './JWTAuthentication'
+
+class AuthenticationFactory {
+  private constructor() {}
+
+  public static make(): IAuthenticationService {
+    const userRepository = UserRepositoryFactory.make()
+    const accessKeyValidation = new AccessKeyValidation()
+    return new Authentication(userRepository, accessKeyValidation)
+  }
+}
+
+export default AuthenticationFactory
