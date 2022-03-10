74e29fb4623869bce1adf98c51e19ad4112d63d7
Yago Biermann <bsfyagobsf@gmail.com>
2022-03-09 23:20:33 -0300

    feat: add room entity

--- server/src/domain/models/room/Room.ts
+++ server/src/domain/models/room/Room.ts
@@ -1,18 +1,33 @@
 import IRoomDTO from './RoomDTO'
-import Identifier from '../../shared/valueObjects/Identifier'
-import RoomCode from '../../shared/valueObjects/RoomCode'
+import RoomId from './RoomId'
+import RoomCode from './RoomCode'
+import Message from './message/Message'
+import UserId from '../user/UserId'
+import RoomError from '../../errors/models/RoomError'
+import Entity from '../common/Entity'
+import IMessageDTO from './message/MessageDTO'
+import UserError from '../../errors/models/UserError'
+import DomainEventPublisher from '../common/DomainEventPublisher'
+import RoomCreatedEvent from './UserAddedEvent'
 
-class Room {
-  private _id: Identifier
+class Room extends Entity {
+  private _id: RoomId
   private _roomCode: RoomCode
-  private _messages: Array<string> = []
-  private _users: Array<string> = []
+  private _messages: Array<Message> = []
+  private _users: Set<string> = new Set()
 
   public constructor(room: IRoomDTO) {
-    this._id = new Identifier(room.id)
-    this._roomCode = new RoomCode(room.roomCode)
-    this._messages = room.messages
-    this._users = room.users
+    super()
+    this._id = new RoomId(room.id)
+    this._roomCode = room.roomCode
+      ? new RoomCode(room.roomCode)
+      : Room.generateRoomCode()
+
+    if (!room.users) {
+      return
+    }
+
+    room.users.forEach(userId => this.addUser(new UserId(userId).value))
   }
 
   public get id(): string {
@@ -23,12 +38,37 @@ class Room {
     return this._roomCode.value
   }
 
-  public get messages(): Array<string> {
+  public get messages(): Array<Message> {
     return this._messages
   }
 
   public get users(): Array<string> {
-    return this._users
+    return Array.from(this._users)
+  }
+
+  public addUser(userId: string) {
+    const fullRoom = this._users.size > 20
+
+    if (fullRoom) {
+      throw new RoomError('ERR_ROOM_FULL')
+    }
+    this._users.add(new UserId(userId).value)
+    DomainEventPublisher.instance().publish(new RoomCreatedEvent(this, userId))
+  }
+
+  public static generateRoomCode(): RoomCode {
+    return new RoomCode()
+  }
+
+  public removeUser(userId: UserId) {
+    const user = this._users.has(userId.value)
+    this.assertArgumentNotNull(userId, new UserError('ERR_USER_NOT_FOUND'))
+    this.assertStateTrue(user, new UserError('ERR_USER_NOT_FOUND'))
+    this._users.delete(userId.value)
+  }
+
+  public addMessage(message: IMessageDTO) {
+    this._messages.push(new Message(message))
   }
 }
 
