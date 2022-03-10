7bbb294dfff3d678305662c5a183404b4c25719d
Yago Biermann <bsfyagobsf@gmail.com>
2022-03-09 21:26:19 -0300

    refactor: add db session and change connect method

--- server/src/infrastructure/database/connection/index.ts
+++ server/src/infrastructure/database/connection/index.ts
@@ -1,12 +1,31 @@
 import mongoose, { Mongoose } from 'mongoose'
 
 class Database {
-  private mongoose: Mongoose = mongoose
+  private _mongoose: Mongoose = mongoose
+  private _session: mongoose.ClientSession
+  private static _instance: Database
 
-  constructor(private _URI: string) {}
+  private constructor() {}
 
-  public async connect(): Promise<void> {
-    await this.mongoose.connect(this._URI)
+  public static instance(): Database {
+    if (!this._instance) {
+      this._instance = new Database()
+    }
+    return this._instance
+  }
+
+  public async session() {
+    this._session = await this._mongoose.startSession()
+    return this._session
+  }
+
+  public async connect(URI: string): Promise<void> {
+    await this._mongoose.connect(URI, {
+      readConcern: { level: 'majority' }
+    })
+    this._mongoose.connection.on('error', () => {
+      process.exit(1)
+    })
   }
 }
 
