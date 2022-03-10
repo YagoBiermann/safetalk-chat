2b8439e78607a7c7cc01ee8948530027c6b71ccf
Yago Biermann <bsfyagobsf@gmail.com>
2022-03-09 23:26:34 -0300

    feat: add auth service

---
+++ server/src/domain/models/auth/AuthenticationService.ts
@@ -0,0 +1,17 @@
+import AuthError from '../../errors/models/AuthError'
+
+interface IAuthenticationInputDTO {
+  userId: string
+  accessKey: string
+}
+
+interface IAuthenticationService {
+  generateAccessKey(
+    data: string,
+    secret: string,
+    expiresIn: string | number
+  ): string
+  authenticate(auth: IAuthenticationInputDTO): Promise<AuthError | null>
+}
+
+export { IAuthenticationService, IAuthenticationInputDTO }
