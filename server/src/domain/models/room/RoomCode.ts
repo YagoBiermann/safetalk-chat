df7a7e1518e08198b2df397ecdf9a4f0005ce98d
Yago Biermann <bsfyagobsf@gmail.com>
2022-03-09 23:20:49 -0300

    feat: add roomCode value object

---
+++ server/src/domain/models/room/RoomCode.ts
@@ -0,0 +1,43 @@
+import RoomError from '../../errors/models/RoomError'
+import short from 'short-uuid'
+import ValueObject from '../common/ValueObject'
+
+class RoomCode extends ValueObject {
+  private readonly _roomCode: string
+
+  constructor(value?: string) {
+    super()
+    if (value) {
+      this.validate(value)
+      this._roomCode = value
+      return
+    }
+    this._roomCode = short.generate()
+  }
+
+  get value(): string {
+    return this._roomCode
+  }
+
+  private validate(value: string): Error | null {
+    const roomCode = short().toUUID(value)
+    this.assertArgumentSuitableWithPattern(
+      roomCode,
+      new RegExp(
+        /^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i
+      ),
+      new RoomError('ERR_INVALID_ROOM_CODE')
+    )
+
+    this.assertArgumentStringLength(
+      value,
+      0,
+      36,
+      new RoomError('ERR_INVALID_ROOM_CODE')
+    )
+
+    return null
+  }
+}
+
+export default RoomCode
