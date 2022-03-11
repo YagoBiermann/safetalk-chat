8bd26ee2f8d194f90e1a727dd1881e1d875485a5
Yago Biermann <bsfyagobsf@gmail.com>
2022-03-11 06:27:32 -0300

    feat: add controller to get all users from room

---
+++ server/src/adapter/controller/GetAllUsersFromRoom.ts
@@ -0,0 +1,29 @@
+import express from 'express'
+import IController from '../ports/controllers/Controller'
+import PresenterFactory from '../presenter/PresenterFactory'
+import { IRoomApplicationService } from '../../application/ports/services/RoomApplicationService'
+
+class GetAllUsersFromRoom implements IController {
+  constructor(private roomApplicationService: IRoomApplicationService) {}
+
+  async handle(router: express.Router): Promise<express.Router> {
+    return router.get('/rooms/current/users', async (req, res) => {
+      const { successPresenter, errorHandler } = PresenterFactory.make(res)
+      const userId = req.session.user
+      const accessKey = req.session.accessKey
+      const roomId = req.session.room
+      try {
+        const users = await this.roomApplicationService.getAllUsersFromRoom({
+          roomId,
+          auth: { accessKey, userId }
+        })
+
+        return successPresenter.success({ users })
+      } catch (error) {
+        return errorHandler.handle(error)
+      }
+    })
+  }
+}
+
+export default GetAllUsersFromRoom
