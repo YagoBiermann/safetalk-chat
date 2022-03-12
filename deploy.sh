faaf43856b5d1f27b49d9d79f6396e18a827a5ac
Yago Biermann <bsfyagobsf@gmail.com>
2022-03-12 02:20:35 -0300

    refactor: change script to accept debug option

--- deploy.sh
+++ deploy.sh
@@ -22,47 +22,55 @@ help()
     echo "Usage example: $arg0 --user johnDoe --password STR0N5P4SS --authenticationDatabase admin"
     echo
     echo "  {-u|--user} user                         -- Set user"
+    echo "  {-d|--debug}                             -- Set debug mode"
     echo "  {-p|--password} password                 -- Set password"
-    echo "  {-d|--authenticationDatabase} database   -- Set database to authenticate"
+    echo "  {-a|--authenticationDatabase} database   -- Set database to authenticate"
     echo "  {-h|--help}                              -- Print this help message and exit"
     exit 0
 }
 
 flags()
 {
-    while test $# -gt 0
-    do
+    while [ $# -gt 0 ]; do
         case "$1" in
-        (-u|--user)
-            shift
-            [ $# = 0 ] && error "No user specified"
-            export USER="$1"
-            shift;;
-        (-p|--password)
-            shift
-            [ $# = 0 ] && error "No password specified"
-            export PWD="$1"
-            shift;;
-        (-d|--authenticationDatabase)
-            shift
-            [ $# = 0 ] && error "No authentication database specified"
-            export DB="$1"
-            shift;;
-        (-h|--help)
-            help;;
-        (*) help;;
+            -u | --user )
+                shift
+                [ $# = 0 ] && error "No user specified"
+                export USER="$1"
+                shift;;
+            -p | --password )
+                shift
+                [ $# = 0 ] && error "No password specified"
+                export PWD="$1"
+                shift;;
+            -adb | --authenticationDatabase )
+                shift
+                [ $# = 0 ] && error "No authentication database specified"
+                export DB="$1"
+                shift;;
+            -d | --debug )
+                [ $# = 0 ] && export DEBUG=false
+                export DEBUG=true
+                shift;;
+            -h | --help )
+                help;;
         esac
     done
 }
 
-flags "$@"
-
-if [ -z "$@" ]; then
-    help
+if [ -z "$1" ]; then
+  help
 fi
 
-docker-compose -f docker-compose.dev.yml --env-file .docker.dev.env -p safetalk up -d
+flags "$@"
+
+docker-compose -f docker-compose.db.dev.yml --env-file .docker.dev.env -p safetalk up -d
 sleep 5
 docker exec -d safetalk_db mongosh --port 27017 --authenticationDatabase ${DB} -u ${USER} -p ${PWD} --file ./scripts/init.js
-sleep 15
-docker restart safetalk_server
+sleep 5
+
+if [ $DEBUG ]; then
+    docker-compose -f docker-compose.server.dev.yml -f docker-compose.server.debug.yml -p safetalk up -d
+else
+    docker-compose -f docker-compose.server.dev.yml -p safetalk up -d
+fi
