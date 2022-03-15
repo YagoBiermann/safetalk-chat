92753abaf1e5e9a077817e48900e686e1317cd17
Yago Biermann <bsfyagobsf@gmail.com>
2022-03-15 13:10:05 -0300

    refactor: remove useless endpoints

--- server/src/infrastructure/express/routes/AppRoutes.ts
+++ server/src/infrastructure/express/routes/AppRoutes.ts
@@ -1,19 +1,13 @@
 import express from 'express'
 import { Request, Response } from 'express'
-import IController from '../../../adapter/ports/controllers/Controller'
-import DomainEventPublisher from '../../../domain/models/common/DomainEventPublisher'
+import IRouteController from '../../../adapter/ports/controllers/RouteController'
 
 class AppRoutes {
   private _router = express.Router()
 
-  constructor(private controllers: Array<IController>) {}
+  constructor(private controllers: Array<IRouteController>) {}
 
-  private endpoint() {
-    this._router.get('rooms/:roomCode/users')
-    this._router.get('rooms/:roomCode/files/stream/:media')
-    this._router.get('rooms/:roomCode/files/:file')
-    this._router.post('rooms/:roomCode/files')
-
+  private redirect() {
     this._router.get('*', (req: Request, res: Response) => {
       res.redirect(302, 'http://safetalk_client:3000')
     })
@@ -24,7 +18,8 @@ class AppRoutes {
   }
 
   public exec() {
-    this.controllers.forEach(controllers => controllers.handle(this._router))
+    this.controllers.forEach(controller => controller.handle(this._router))
+    this.redirect()
   }
 }
 
