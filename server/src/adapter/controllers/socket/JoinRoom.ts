643ef9511ade0fd7d53202a4479b6717e22e3d40
Yago Biermann <bsfyagobsf@gmail.com>
2022-03-17 13:07:17 -0300

    fix: add missing curly brackets

--- server/src/adapter/controllers/socket/JoinRoom.ts
+++ server/src/adapter/controllers/socket/JoinRoom.ts
@@ -3,7 +3,7 @@ import ISocketController from '../../ports/controllers/SocketController'
 
 class JoinRoomEventController implements ISocketController {
   public async handle(socket: Socket): Promise<Socket> {
-    return socket.on('room:join', async (roomCode: string) => {
+    return socket.on('room:join', async ({ roomCode }) => {
       console.log(`${socket.id} joined room ${roomCode}`)
       socket.join(roomCode)
     })
