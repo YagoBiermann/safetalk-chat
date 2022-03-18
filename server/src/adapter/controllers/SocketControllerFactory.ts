da94a61f40f85ca809df34135baf2bf4e6d872b1
Yago Biermann <bsfyagobsf@gmail.com>
2022-03-18 01:13:42 -0300

    feat: add factory for UserDisconnectEventController

--- server/src/adapter/controllers/SocketControllerFactory.ts
+++ server/src/adapter/controllers/SocketControllerFactory.ts
@@ -1,9 +1,16 @@
+import { IUserApplicationService } from '../../application/ports/services/UserApplicationService'
+import ApplicationServiceFactory from '../../application/services/ApplicationServiceFactory'
 import ISocketController from '../ports/controllers/SocketController'
 import GetAllUsersFromRoomEventController from './socket/GetAllUsersFromRoom'
 import JoinRoomEventController from './socket/JoinRoom'
+import UserDisconnectEventController from './socket/UserDisconnect'
 
 class SocketControllerFactory {
   private constructor() {}
+
+  private static userApplicationService(): IUserApplicationService {
+    return ApplicationServiceFactory.makeUserApplicationService()
+  }
 
   public static makeJoinRoomEventController(): ISocketController {
     const joinRoomEventController = new JoinRoomEventController()
@@ -16,6 +23,14 @@ class SocketControllerFactory {
       new GetAllUsersFromRoomEventController()
 
     return getAllUsersFromRoomEventController
+  }
+
+  public static makeUserDisconnectEventController(): ISocketController {
+    const userDisconnectEventController = new UserDisconnectEventController(
+      this.userApplicationService()
+    )
+
+    return userDisconnectEventController
   }
 }
 
