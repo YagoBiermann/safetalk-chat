abda51e6ee2b9764c2ffe46a6547a1788e729f70
Yago Biermann <bsfyagobsf@gmail.com>
2022-03-27 22:34:13 -0300

    refactor: remove acceptedTypes enum

--- client/src/lib/enums/index.ts
+++ client/src/lib/enums/index.ts
@@ -13,24 +13,3 @@ export enum ENDPOINTS {
   FRONTEND_URL = 'http://localhost:80/',
   NGINX_URL = 'http://safetalk_nginx:80/api/v2/'
 }
-
-//accepted file types for upload
-export const acceptedTypes = `
-  image/jpeg, 
-  image/png, 
-  video/webm, 
-  video/ogg, 
-  video/quicktime,
-  video/mp4, 
-  video/wmv, 
-  video/avi, 
-  video/mov,
-  audio/mp3,
-  audio/mp4, 
-  text/plain, 
-  application/pdf, 
-  application/zip, 
-  application/vnd.openxmlformats-officedocument.wordprocessingml.document, 
-  application/msword, application/vnd.openxmlformats-officedocument.presentationml.presentation, 
-  application/vnd.ms-powerpoint
-`
