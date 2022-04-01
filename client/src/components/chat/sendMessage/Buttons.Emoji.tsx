27a7a38c5fc9b4ffbb838f9142129aca19f8d917
Yago Biermann <bsfyagobsf@gmail.com>
2022-04-01 13:17:07 -0300

    feat: add event to open/close the emoji picker

--- client/src/components/chat/sendMessage/Buttons.Emoji.tsx
+++ client/src/components/chat/sendMessage/Buttons.Emoji.tsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Button, ButtonProps } from '@mui/material'
 import EmojiEmotions from '@material-ui/icons/EmojiEmotions'
 import { motion } from 'framer-motion'
 import { ButtonAnimation } from './SendMessage.Animations'
+import { emojiContext } from '../../../lib/context/emojiContext'
 
 const EmojiButton = (props: ButtonProps) => {
+  const { isEmojiPickerOpen, setEmojiPickerOpen } = useContext(emojiContext)
+
   return (
-    <Button variant="send-message-button" color="primary" {...props}>
+    <Button
+      id="emojiButton"
+      onClick={() => setEmojiPickerOpen(!isEmojiPickerOpen)}
+      variant="send-message-button"
+      color="primary"
+      {...props}
+    >
       <EmojiEmotions
         component={motion.svg}
         fontSize="medium"
