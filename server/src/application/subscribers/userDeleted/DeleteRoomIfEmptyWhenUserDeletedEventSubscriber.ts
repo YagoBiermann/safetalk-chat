d3e1d7f12911a1035344db7283b63f8d98a44242
Yago Biermann <bsfyagobsf@gmail.com>
2022-04-03 16:41:29 -0300

    feat: delete objects from aws when room is deleted

--- server/src/application/subscribers/userDeleted/DeleteRoomIfEmptyWhenUserDeletedEventSubscriber.ts
+++ server/src/application/subscribers/userDeleted/DeleteRoomIfEmptyWhenUserDeletedEventSubscriber.ts
@@ -1,11 +1,15 @@
 import IDomainEventSubscriber from '../../../domain/models/common/DomainEventSubscriber'
 import UserDeletedEvent from '../../../domain/events/UserDeletedEvent'
 import { IRoomRepository } from '../../../domain/models/room/RoomRepository'
+import ICloudService from '../../ports/services/CloudService'
 
 class DeleteRoomIfEmptyWhenUserDeletedEventSubscriber
   implements IDomainEventSubscriber<UserDeletedEvent>
 {
-  constructor(private _roomRepository: IRoomRepository) {}
+  constructor(
+    private roomRepository: IRoomRepository,
+    private cloudService: ICloudService
+  ) {}
   eventName(): string {
     return UserDeletedEvent.name
   }
@@ -16,14 +20,15 @@ class DeleteRoomIfEmptyWhenUserDeletedEventSubscriber
   public async handleEvent(anEvent: UserDeletedEvent): Promise<void> {
     try {
       const user = anEvent.userId
-      const room = await this._roomRepository.getRoomById(anEvent.roomId)
+      const room = await this.roomRepository.getRoomById(anEvent.roomId)
       const hasUsers = room.users.length > 1 // 1 represents the last user who left the room
       if (hasUsers) {
         room.disconnect(user)
-        await this._roomRepository.save(room)
+        await this.roomRepository.save(room)
         return
       }
-      await this._roomRepository.delete(room.id)
+      await this.roomRepository.delete(room.id)
+      await this.cloudService.deleteDirectory(room.roomCode)
     } catch (error) {
       throw error
     }
