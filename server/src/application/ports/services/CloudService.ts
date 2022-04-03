3ef92ef8769e344f0e6a7f547c8af68865d5223d
Yago Biermann <bsfyagobsf@gmail.com>
2022-04-03 16:39:21 -0300

    refactor: remove not used method

--- server/src/application/ports/services/CloudService.ts
+++ server/src/application/ports/services/CloudService.ts
@@ -2,7 +2,6 @@ interface ICloudService {
   storage: AWS.S3
   getSignedCookie(roomCode: string): AWS.CloudFront.Signer.CustomPolicy
   getSignedUrl(fileName: string): string
-  uploadFile(file: Express.Multer.File, roomCode: string): Promise<void>
   deleteDirectory(roomCode: string): Promise<void>
 }
 
--- server/src/infrastructure/aws/AWSManager.ts
+++ server/src/infrastructure/aws/AWSManager.ts
@@ -1,7 +1,6 @@
 import AWS from 'aws-sdk'
 import fs from 'fs'
 import path from 'path'
-import { v4 as uuidv4 } from 'uuid'
 import ICloudService from '../../application/ports/services/CloudService'
 
 class AWSManager implements ICloudService {
@@ -50,18 +49,6 @@ class AWSManager implements ICloudService {
   }
 
   public getSignedUrl(fileName: string) {
-    const policy = JSON.stringify({
-      Statement: [
-        {
-          Resource: `https://${process.env.AWS_CLOUDFRONT_DOMAIN}/${fileName}`,
-          Condition: {
-            DateLessThan: {
-              'AWS:EpochTime': Math.floor(Date.now() / 1000) + 60 * 60 * 24 * 7 // 1 week
-            }
-          }
-        }
-      ]
-    })
     const privateKeyPath = path.join(__dirname, 'private_aws_key.pem')
     const privateKey = fs.readFileSync(privateKeyPath, 'utf8')
     const cookie = new AWS.CloudFront.Signer(
@@ -74,22 +61,6 @@ class AWSManager implements ICloudService {
       url: `https://${process.env.AWS_CLOUDFRONT_DOMAIN}/${fileName}`
     })
     return signedCookie
-  }
-
-  public async uploadFile(file: Express.Multer.File, roomCode: string) {
-    const fileExt = path.extname(file.originalname)
-    const fileName = `${roomCode}/${uuidv4()}.${fileExt}`
-
-    try {
-      const params = {
-        Bucket: process.env.AWS_BUCKET,
-        Key: fileName,
-        Body: file
-      }
-      await this._S3Storage.putObject(params).promise()
-    } catch (error) {
-      throw error
-    }
   }
 
   public async deleteDirectory(roomCode: string) {
