527dab1ad56a28e02788cd37272eafe05a21485d
Yago Biermann <bsfyagobsf@gmail.com>
2022-04-08 22:32:15 -0300

    feat: add production script

--- stop.sh
+++ stop.sh
@@ -1,1 +1,47 @@
-docker-compose -f docker-compose.db.dev.yml -f docker-compose.server.dev.yml --env-file .docker.dev.env -p safetalk down
+#!/bin/sh
+
+arg0=$(basename "$0" .sh)
+
+help()
+{
+    echo
+    echo "Usage example: $arg0 --dev"
+    echo
+    echo "  {-prod|--production}                  -- Stop production containers"
+    echo "  {-dev|--development}                  -- Stop development containers"
+    echo "  {-h|--help}                           -- Show this help message and exit"
+    exit 0
+}
+
+
+flags()
+{
+    while [ $# -gt 0 ]; do
+        case "$1" in
+            -prod | --production )
+                [ $# = 0 ] && export PRODUCTION=false
+                export PRODUCTION="$1"
+                shift;;
+            -dev | --development )
+                [ $# = 0 ] && export DEVELOPMENT=false
+                export DEVELOPMENT="$1"
+                shift;;
+            -h | --help )
+                help;;
+        esac
+    done
+}
+
+if [ -z "$1" ]; then
+  help
+fi
+
+flags "$@"
+
+if [ $DEVELOPMENT ]; then
+  docker-compose -f docker-compose.db.base.yml -f docker-compose.dev.yml -f docker-compose.base.yml --env-file .docker.dev.env -p safetalk down
+fi
+
+if [ $PRODUCTION ]; then
+  docker-compose -f docker-compose.db.base.yml -f docker-compose.prod.yml -f docker-compose.base.yml --env-file .docker.prod.env -p safetalk down
+fi
