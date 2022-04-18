b09c2690d0ef6d6d0eb262aac09e825ec5099854
Yago biermann T <bsfyagobsf@gmail.com>
2022-04-18 03:04:27 -0300

    test: add unit tests for class FileMessage

---
+++ server/__tests__/domain/models/room/message/FileMessage.test.ts
@@ -0,0 +1,17 @@
+import { describe, expect, beforeEach, test, jest } from '@jest/globals'
+import FileMessage from '../../../../../src/domain/models/room/message/FileMessage'
+
+describe('Tests on class FileMessage', () => {
+  test('Should create a file message', () => {
+    const fileMessage = new FileMessage(
+      'http://localhost:3000/file/1',
+      'file.txt',
+      'text/plain',
+      100
+    )
+    expect(fileMessage.url).toBe('http://localhost:3000/file/1')
+    expect(fileMessage.name).toBe('file.txt')
+    expect(fileMessage.type).toBe('text/plain')
+    expect(fileMessage.size).toBe(100)
+  })
+})
