4003b646c6816103782186febaf51edaaf2e4618
Yago biermann T <bsfyagobsf@gmail.com>
2022-04-18 17:26:46 -0300

    test: add unit tests for enum MessageType

--- server/__tests__/domain/models/room/message/MessageType.test.ts
+++ server/__tests__/domain/models/room/message/MessageType.test.ts
@@ -2,9 +2,11 @@ import { describe, expect, test } from '@jest/globals'
 import MessageType from '../../../../../src/domain/models/room/message/MessageType'
 
 describe('Tests on enum MessageType', () => {
-  expect(MessageType.TEXT).toBe('TEXT')
-  expect(MessageType.FILE).toBe('FILE')
-  expect(MessageType.AUDIO).toBe('AUDIO')
-  expect(MessageType.VIDEO).toBe('VIDEO')
-  expect(MessageType.IMAGE).toBe('IMAGE')
+  test('Should match a message type', () => {
+    expect(MessageType.TEXT).toBe('TEXT')
+    expect(MessageType.FILE).toBe('FILE')
+    expect(MessageType.AUDIO).toBe('AUDIO')
+    expect(MessageType.VIDEO).toBe('VIDEO')
+    expect(MessageType.IMAGE).toBe('IMAGE')
+  })
 })
