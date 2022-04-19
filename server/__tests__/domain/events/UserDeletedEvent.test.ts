8d5a481eef2718c0df65db78b155d82a0aac1ea1
Yago biermann T <bsfyagobsf@gmail.com>
2022-04-18 22:18:52 -0300

    test: add unit tests for class UserDeletedEvent

---
+++ server/__tests__/domain/events/UserDeletedEvent.test.ts
@@ -0,0 +1,15 @@
+import { describe, expect, beforeEach, test, jest } from '@jest/globals'
+import UserDeletedEvent from '../../../src/domain/events/UserDeletedEvent'
+import { v4 as uuidv4 } from 'uuid'
+
+describe('Tests on class UserDeletedEvent', () => {
+  test('should create an user deleted event', () => {
+    const roomId = uuidv4()
+    const userId = uuidv4()
+    const event = new UserDeletedEvent(roomId, userId)
+    expect(event.roomId).toBe(roomId)
+    expect(event.userId).toBe(userId)
+    expect(event.occurredOn()).toBeInstanceOf(Date)
+    expect(event.eventName()).toBe(UserDeletedEvent.name)
+  })
+})
