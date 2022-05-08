49e156c7ea08fa3fef18670de39d3070cc3a889a
Yago biermann T <bsfyagobsf@gmail.com>
2022-05-08 13:18:12 -0300

    build: disable strictMode from nextJs

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -2,7 +2,7 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   poweredByHeader: false,
   typescript: {
     ignoreBuildErrors: true
