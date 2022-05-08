7170b26cc491956fbe4a2fd7f1a0d02bdc356119
Yago biermann T <bsfyagobsf@gmail.com>
2022-05-08 17:41:38 -0300

    test: add tests on class SingleTransaction

---
+++ server/__tests__/infrastructure/database/SingleTransaction.test.ts
@@ -0,0 +1,97 @@
+import {
+  describe,
+  expect,
+  beforeAll,
+  afterAll,
+  beforeEach,
+  test,
+  jest
+} from '@jest/globals'
+import { v4 as uuidv4 } from 'uuid'
+import UserRepository from '../../../src/infrastructure/database/repositories/UserRepository'
+import RoomRepository from '../../../src/infrastructure/database/repositories/RoomRepository'
+import { Database } from '../../../src/infrastructure/database/connection'
+import UserMapper from '../../../src/infrastructure/database/mapper/UserMapper'
+import RoomMapper from '../../../src/infrastructure/database/mapper/RoomMapper'
+import User from '../../../src/domain/models/user/User'
+import Room from '../../../src/domain/models/room/Room'
+import { User as UserModel } from '../../../src/infrastructure/database/models/users'
+import { Room as RoomModel } from '../../../src/infrastructure/database/models/rooms'
+import SingleTransaction from '../../../src/infrastructure/database/repositories/SingleTransaction'
+
+describe('tests on class SingleTransaction', () => {
+  const userMapper = new UserMapper()
+  const roomMapper = new RoomMapper()
+  const userRepository = new UserRepository(userMapper)
+  const roomRepository = new RoomRepository(roomMapper)
+  const singleTransaction = new SingleTransaction(
+    roomRepository,
+    userRepository
+  )
+
+  beforeEach(async () => {
+    await UserModel.deleteMany({})
+    await RoomModel.deleteMany({})
+    jest.clearAllMocks()
+  })
+
+  beforeAll(async () => {
+    await Database.instance()
+      .connect(process.env.MONGO_TEST_URI)
+      .then(() => {
+        console.log('Connected to MongoDB')
+      })
+  })
+  afterAll(async () => {
+    await Database.instance()
+      .disconnect()
+      .then(() => {
+        console.log('Disconnected from MongoDB')
+      })
+  })
+
+  const spyOnRoomRepositorySave = jest.spyOn(roomRepository, 'save')
+  const spyOnUserRepositorySave = jest.spyOn(userRepository, 'save')
+  const spyOnStartTransaction = jest.spyOn(
+    Database.instance(),
+    'startTransaction'
+  )
+  const spyOnCommitTransaction = jest.spyOn(
+    Database.instance(),
+    'commitTransaction'
+  )
+  const spyOnAbortTransaction = jest.spyOn(
+    Database.instance(),
+    'abortTransaction'
+  )
+  const spyOnSession = jest.spyOn(Database.instance(), 'session')
+
+  test('should save all', async () => {
+    const user = new User({ id: null, username: 'JohnDoe' })
+    const room = new Room({ id: null, roomCode: Room.generateRoomCode().value })
+    await singleTransaction.saveAll(user, room)
+    expect(spyOnRoomRepositorySave).toHaveBeenCalled()
+    expect(spyOnUserRepositorySave).toHaveBeenCalled()
+    expect(spyOnStartTransaction).toHaveBeenCalled()
+    expect(spyOnCommitTransaction).toHaveBeenCalled()
+    expect(spyOnAbortTransaction).not.toHaveBeenCalled()
+    expect(spyOnSession).toHaveBeenCalled()
+  })
+
+  test('should abort the transaction', async () => {
+    const user = new User({ id: null, username: 'JohnDoe' })
+    const room = new Room({ id: null, roomCode: Room.generateRoomCode().value })
+    const result = jest.fn(async () => {
+      // @ts-ignore
+      return await singleTransaction.saveAll(user, 'error')
+    })
+    await result().catch(() => {
+      expect(spyOnStartTransaction).toHaveBeenCalled()
+      expect(spyOnCommitTransaction).not.toHaveBeenCalled()
+      expect(spyOnAbortTransaction).toHaveBeenCalled()
+      expect(spyOnSession).toHaveBeenCalled()
+      expect(spyOnRoomRepositorySave).toHaveBeenCalled()
+      expect(spyOnUserRepositorySave).not.toHaveBeenCalled()
+    })
+  })
+})
